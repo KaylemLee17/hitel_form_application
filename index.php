635739cf22845ca4cdf419ea9e815fde8dfb54dc
Kaylem-Lee Andrews <42963595+KaylemLee17@users.noreply.github.com>
2019-02-01 13:38:55 +0200

    added js and changed some css

--- index.php
+++ index.php
@@ -7,36 +7,43 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.7.2/css/bulma.min.css" /> -->
     <link rel="stylesheet" href="css/main.css">
-    <script src="main.js"></script>
+    <script src="/js/main.js"></script>
 </head>
 <body>
-    <div class="heading">
-        <h2>Online Hotel Booking Form</h2>
-    </div>
-    <div class="form">
-        <form action="index.php" method="post">
-            <div>
-                <p>Name:</p>
-                <input type="text" name="Name" class="name" placeholder="Name">
-            </div>
-            <div>
-                <p>Surname:</p>
-                <input type="text" name="Surname" class="name" placeholder="Surname">
-            </div>
-            <div>
-                <p>Number of nights:</p>
-                <input type="number" name="number" id="">
-            </div>
-            <div>
-                <p>Name of Hotel:</p>
-                <select name="Hotel name" id="" disabled="disabled">-Select one-
-                    <option value=""></option>
-                    <option value=""></option>
-                    <option value=""></option>
-                </select>
-            </div>
-            <button type="submit" name="submit" class="add_btn">Submit</button>
-        </form>
+    <div>
+        <div class="heading">
+            <h2>Online Hotel Booking Form</h2>
+        </div>
+        <div class="form">
+            <form action="index.php" method="post">
+                <div>
+                    <p>Name:</p>
+                    <input type="text" name="Name" class="form" placeholder="Name" required>
+                </div>
+                <div>
+                    <p>Surname:</p>
+                    <input type="text" name="Surname" class="form" placeholder="Surname" reqired>
+                </div>
+                <div>
+                    Check in date:
+                    <input type="date" name="date" id="date" class="form">
+                    <br>
+                    Check out date:
+                    <input type="date" name="date" id="date" class="form">
+                </div>
+                <div class="dropdown">
+                    <button onclick="myFunction()" class="dropbtn">--Select Hotel--</button>
+                    <div id="myDropdown" class="dropdown-content">
+                        <a href="#">Link 1</a>
+                        <a href="#">Link 2</a>
+                        <a href="#">Link 3</a>
+                    </div>
+                </div> 
+                <br>
+                <button type="submit" name="submit" class="submit_btn">Submit</button>
+            </form>
+        
     </div>
 </body>
 </html>
+
